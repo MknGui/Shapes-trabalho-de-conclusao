1a4b2356ee05031bba2fe8a6ee46c8a8a196410a
gseisinger <119032169+gseisinger@users.noreply.github.com>
2022-11-25 04:07:11 -0300

    Update and rename App.java to TestDraw.java

---
+++ TestDraw.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+	 public class TestDraw{
+
+public static void main( String[] args ){
+
+DrawPanel panel = new DrawPanel();
+DFrame application = new JFrame()
+application.setDefaultCloseOperation ( JFrame.EXIT_ON CLOSE ) ;
+application.add (panel) ;
+application.setSize (300,300);
+application.setVisible (true); 
+	} 
+} 
